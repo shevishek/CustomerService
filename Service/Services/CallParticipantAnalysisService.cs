d20cd4a08f2181ce553e8259922af5e6f89d52a3
shevishek <shevishek@gmail.com>
2026-02-11 13:55:54 +0200

    Tokens

--- Service/Services/CallParticipantAnalysisService.cs
+++ Service/Services/CallParticipantAnalysisService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Repository.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace Service.Services
 {
-    internal class CallParticipantAnalysisService
+
+    public class CallParticipantAnalysisService
     {
+        private readonly IRepository<CallParticipantAnalysis> repository;
+        private readonly IMapper mapper;
+
     }
 }
